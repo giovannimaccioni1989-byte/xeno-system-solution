ae932ae56f7fb628e09b657cad595804e7875a43
giovannimaccioni1989-byte <giovannimaccioni1989@gmail.com>
2026-06-17 16:05:30 +0200

    Update index.js

--- api/market-ticker/index.js
+++ api/market-ticker/index.js
@@ -1,5 +1,8 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const apiKey = process.env.FINNHUB_API_KEY
+
   return res.status(200).json({
-    status: 'API working'
+    apiKeyExists: !!apiKey,
+    apiKeyLength: apiKey ? apiKey.length : 0
   })
 }
